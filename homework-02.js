d17f84b6314f10c2242462adf7210575ef0a4d86
Your Name <you@example.com>
2021-09-13 21:52:41 -0300

    Finish Homework02

---
+++ homework-02.js
@@ -0,0 +1,7 @@
+Cuando creamos una variable lo que estamos creando es un elemento que vamos a poder utilizar mas tarde. A estas variables las vamos a nombrar y le vamos a agregar el valor que querramos.
+Llamamos string o cadena a un tipo de dato que cendria a ser como un conjunto de caracteres, ya sean letras o numeros, y vamos a poder darnos cuenta que es una string ya que si o si tienen que estar encerradas entre comillas.
+La Funcion es una herramienta que nos va a permitir hacer mas facil los procesos, ya que vamos a poder tener dentro de ellas un conjunto de variables que se van a ejecutar deacuardo a los valores que le demos anteriormente, las funciones solo nos van a mostrar el resultado que nosotros hallamos pedido que nos muestre al momento de crearla gracias al return 
+La declaracion if nos permite hacer que una funcion evalue una variable con una constante y deacuerdo al resultado realice una accion u otra 
+Los valores booleanos es un tipo de dato que nos va a ayudar a darle significado a la evaluacion entre dos elementos. y nos va a dar como resultado verdadero o falso 
+
+
