2bc6655ece4c1b34ebee24951e80c3684cd94012
Your Name <you@example.com>
2021-09-14 21:05:15 -0300

    Homework-03.js

---
+++ homework-03.js
@@ -0,0 +1,7 @@
+For: Los softwares se ejecutan en bucles, estos bucles evaluan una y otra vez expresiones hasta que conseguimos lo que estamos buscando.
+Existen dos tipos de bucles en JavaScript y For es uno de estos
+Si utilizamos correctamente la sintaxis del bucle FOR haremos que este evalue la expresion condicional que tenemos dentro del parentesis hasta que este de como resultado false, cuando esto ocfurra dejara de evaluarla y se terminara el bucle.
+
+&&: Es un operador logico que significa 'Y' o 'AND' Este evalua las expresiones y devuelve true si las dos expresiones son true. Si una de las expresiones no es verdadera, nos devolvera false. 
+||: Es un operador logico que significa 'O' o 'OR', Este evalua las expresiones y devuelve true si una de las expresiones es true. La unica forma de que este operador nos devuelva false es que ambas expresiones sean falsas
+!: Es un operador logico que significa 'NO' o 'NOT', Este deolvera el valor booleano inverso al que nos daria originalmente. Si algo es true, nos devolvera false y viceversa.
